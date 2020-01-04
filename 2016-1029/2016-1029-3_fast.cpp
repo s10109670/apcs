a4780c77f2226faf9e929dd701987461b5de72b2
s10109670 <b03902075@ntu.edu.tw>
2020-01-04 11:49:18 +0800

    0104

--- 2016-1029/2016-1029-3_fast.cpp
+++ 2016-1029/2016-1029-3_fast.cpp
@@ -4,10 +4,10 @@ using namespace std;
 int main(){
 	int N, M, K;
 	cin >> N >> M >> K;
-	int alive = 0; //ans is peoele[0] at the beginning of ronun-(K+1)
-	/* from round-K+1 think back to round-0 */
-	for(int i = K+1; i > 0; i--){
-		alive = (alive + M) % (N-i+1);
+	int alive = 0; //ans is peoele[0] at the beginning of ronud-(K+1)
+	/* from round-(K+1) think back to round-1 */
+	for(int i = K; i > 0; i--){
+		alive = (alive + M) % (N-i+1); // alive become the pos of round-(i)
 		//cout << "goes to pos:" << alive << endl;
 	}
 	cout << alive+1 << endl;
