1995d69696190f1501e2d754a54364fa221eb4a0
s10109670 <b03902075@ntu.edu.tw>
2020-01-04 09:56:44 +0800

    with_annotation

--- 2016-1029/2016-1029-3_fast.cpp
+++ 2016-1029/2016-1029-3_fast.cpp
@@ -4,9 +4,10 @@ using namespace std;
 int main(){
 	int N, M, K;
 	cin >> N >> M >> K;
-	int alive = 0;
-	for(int i = N-K+1; i <= N; i++){
-		alive = (alive + M) % i;
+	int alive = 0; //ans is peoele[0] at the beginning of ronun-(K+1)
+	/* from round-K+1 think back to round-0 */
+	for(int i = K+1; i > 0; i--){
+		alive = (alive + M) % (N-i+1);
 		//cout << alive;
 	}
 	cout << alive+1 << endl;
